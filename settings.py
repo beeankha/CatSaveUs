2cec26dff4021aefa14cf4fd19347609f9c226e8
beeankha <bstohrer@redhat.com>
2020-11-08 10:29:08 -0500

    Add a clickable Play button

--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
+import pygame
+
 class Settings:
     """A class to store all settings for "Cat Save Us!" game."""
 
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1200
+        self.screen_width = 1350
         self.screen_height = 800
         # ^^ (1200, 800) is a tuple that defines dimensions of game window, in pixels.
         # The object we assigned to self.screen is called a "surface". Each element
